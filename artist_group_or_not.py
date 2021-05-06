30ca72814c3dd60e846608edd820be88e082ece1
divertome <hans9645@gmail.com>
2021-05-06 17:43:23 +0900

    melon 검색 후 그룹인지 아닌지 들고오는 코드 150개에 아이피 차단당함 ㅋㅋ

--- artist_group_or_not.py
+++ artist_group_or_not.py
@@ -37,36 +37,41 @@ with open('singer.csv', 'r') as f:
     # 헤더(컬럼명) 건너뛰고 싶을 때
     #next(csvreader)
     for row in csvreader:
+        elem = browser.find_element_by_xpath("//*[@id='top_search']")
         print(type(row[0]))
         print(row[0])
         payload=[]
         payload.append(row[0])
-        elem = browser.find_element_by_xpath("//*[@id='top_search']")
         elem.send_keys(row[0])
+        #elem.submit()
         elem.send_keys(Keys.RETURN)
         html = browser.page_source
         soup = BeautifulSoup(html, "lxml")
         flag = soup.find('dl', attrs={'class': 'info_02 clfix'})
+        if flag==None:
+            elem = browser.find_element_by_xpath("//*[@id='top_search']")
+            elem.clear()
+            browser.back()
+            continue
         print(flag.find_all('dd')[1].get_text())
         line = flag.find_all('dd')[1].get_text()
+        elem = browser.find_element_by_xpath("//*[@id='top_search']")
+        elem.clear()
         try:
             if line.find(s)!=-1:
                 payload.append(s)
                 writer.writerow(payload)
-                browser.quit()
-                browser.get(url_melon)
-                # elem.clear()
-                # browser.back()
+                elem.clear()
+                browser.back()
                 continue
             elif line.find(g)!=-1:
                 payload.append(g)
                 writer.writerow(payload)
-                browser.quit()
-                browser.get(url_melon)
-                # elem.clear()
-                # browser.back()
+                elem.clear()
+                browser.back()
                 continue
             else:
+                payload.append(" ")
                 writer.writerow(payload)
                 elem.clear()
                 browser.back()
